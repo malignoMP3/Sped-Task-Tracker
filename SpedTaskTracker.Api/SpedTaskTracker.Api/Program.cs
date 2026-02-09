777809c94a293ac4600406a2a59c985854f56066
Maligno <caio.gabrielpdn@gmail.com>
2026-02-08 23:06:27 -0300

    fix: Ajustando CORS para porta fixa do frontend

--- SpedTaskTracker.Api/SpedTaskTracker.Api/Program.cs
+++ SpedTaskTracker.Api/SpedTaskTracker.Api/Program.cs
@@ -16,6 +16,19 @@ builder.Services.AddControllers()
     });
 
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("SpedTaskDb"));
 
@@ -27,6 +40,8 @@ builder.Services.AddScoped<ITaskService, TaskService>();
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngular");
 
 app.UseAuthorization();
 
